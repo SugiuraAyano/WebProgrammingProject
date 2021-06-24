b81a8956ec8ff97d40454465a254209c2581622f
SUGIURAAYANO-MS\SugiuraAyano <ousi4706@gmail.com>
2021-06-25 03:15:17 +0800

    remove cache serializr path settings

--- include/xss.php
+++ include/xss.php
@@ -7,7 +7,7 @@ function xsspurify($InString) {
     // configuration goes here:
     $config->set('Core.Encoding', 'UTF-8'); // replace with your encoding
     $config->set('HTML.Doctype', 'XHTML 1.0 Transitional'); // replace with your doctype
-    $config->set('Cache.SerializerPath', '/tmp');
+    // $config->set('Cache.SerializerPath', '/tmp');
     $purifier = new HTMLPurifier($config);
     return $purifier->purify($InString);
 //   return htmlspecialchars($InString);
