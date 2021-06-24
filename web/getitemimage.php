41767457a02e18782ccb2b1d7d036e1ee8950eff
SUGIURAAYANO-MS\SugiuraAyano <ousi4706@gmail.com>
2021-06-25 03:22:00 +0800

    新增擷取圖片

---
+++ web/getitemimage.php
@@ -0,0 +1,20 @@
+<?php
+require_once("../include/config.php");
+require_once("../include/db_func.php");
+require_once("../include/gpsvars.php");
+$db_conn = connect2db($dbhost, $dbuser, $dbpwd, $dbname);
+
+if (!isset($ID)) 
+    exit();
+$sqlcmd = "SELECT * FROM Item WHERE id='$ID'";
+$rs = querydb($sqlcmd, $db_conn);
+if (count($rs)>0) {
+    $Image = $rs[0]['itemimage'];
+    $filename = $ID . '.png';
+    $filetype = 'application/x-png';
+    header("Content-type: $filetype \n");
+    header("Content-Disposition: filename=$filename \n");
+    echo $Image;
+} else die ('File Not Exist!');
+
+?>
